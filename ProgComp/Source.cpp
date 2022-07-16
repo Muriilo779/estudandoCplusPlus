64e7309a6bd3aa46ef055dd6f2349da47e510b60
unknown <murilohue779@gmail.com>
2022-07-15 22:38:04 -0300

    Entendendo algumas propriedades das Strings no C++

--- ProgComp/Source.cpp
+++ ProgComp/Source.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int somaVetor(int[]);
-
 int main() {
-	int batatas[3] = { 7, 6, 8 };
-	cout << "Total de batatas = ";
-	cout << somaVetor(batatas) << endl;
+	char cat[5] = { 'm', 'i', 'a', 'r', '\0' };
+	char dog[5] = { 'l', 'a', 't', 'i', 'r' };
 
-	system("pause");
-	return 0;
-}
-
-int somaVetor(int vet[]) {
-	return vet[0] + vet[1] + vet[2];
+	cout << cat << endl;
+	cout << dog << endl;
 }
